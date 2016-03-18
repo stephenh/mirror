d9ac817decc3e996c80721264d11bbd917ccd300
Stephen Haberman <stephen.haberman@gmail.com>
2016-03-18 15:37:13 -0500

    Playing around with airline CLI.

--- src/main/java/mirror/Mirror.java
+++ src/main/java/mirror/Mirror.java
@@ -1,5 +1,15 @@
 package mirror;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import io.airlift.airline.Command;
+import io.airlift.airline.HelpOption;
+import io.airlift.airline.Option;
+import io.airlift.airline.SingleCommand;
+
+// Doesn't work yet, run MirrorServer and MirrorClient directly.
 public class Mirror {
 
   static {
@@ -7,8 +17,26 @@ public class Mirror {
   }
 
   public static void main(String[] args) throws Exception {
-    MirrorServer.main(new String[] { "/home/stephen/dir1", "10000" });
-    MirrorClient.main(new String[] { "/home/stephen/dir2", "localhost", "10000" });
+    // MirrorServer.main(new String[] { "/home/stephen/dir1", "10000" });
+    // MirrorClient.main(new String[] { "/home/stephen/dir2", "localhost", "10000" });
+    MirrorCommand c = SingleCommand.singleCommand(MirrorCommand.class).parse(args);
+    if (c.help.showHelpIfRequested()) {
+      return;
+    }
+    c.run();
+  }
+
+  @Command(name = "mirror", description = "two-way real-time sync")
+  public static class MirrorCommand {
+    @Inject
+    public HelpOption help;
+
+    @Option(name = { "-f" }, description = "filters")
+    public List<String> filters;
+
+    public void run() {
+      System.out.println("filters=" + filters);
+    }
   }
 
 }
