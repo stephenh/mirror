203a49de49c76a1c66d1f64daf350ced51c69e8f
Stephen Haberman <stephen.haberman@gmail.com>
2017-07-21 17:13:09 -0700

    Add some connection detector debug code.

--- src/main/java/mirror/ConnectionDetector.java
+++ src/main/java/mirror/ConnectionDetector.java
@@ -5,6 +5,9 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import io.grpc.Channel;
+import io.grpc.netty.NegotiationType;
+import io.grpc.netty.NettyChannelBuilder;
 import io.grpc.stub.StreamObserver;
 import mirror.MirrorGrpc.MirrorStub;
 
@@ -34,29 +37,42 @@ public interface ConnectionDetector {
 
   /** A detector that uses our app-specific PingRequest/PingResponse. */
   public static class Impl implements ConnectionDetector {
-    private static final Duration durationBetweenDetections = Duration.ofSeconds(10);
+    private static final Duration durationBetweenDetections = Duration.ofSeconds(5);
+
+    public static void main(String[] args) throws Exception {
+      Channel c = NettyChannelBuilder.forAddress("shaberma-ld1", 49172).negotiationType(NegotiationType.PLAINTEXT).build();
+      while (true) {
+        new Impl().blockUntilConnected(MirrorGrpc.newStub(c).withCompression("gzip"));
+        System.out.println("CONNECTED");
+        Thread.sleep(5000);
+      }
+    }
 
     @Override
     public boolean isAvailable(MirrorStub stub) {
       AtomicBoolean available = new AtomicBoolean(false);
       CountDownLatch done = new CountDownLatch(1);
-      stub.ping(PingRequest.newBuilder().build(), new StreamObserver<PingResponse>() {
+      // System.out.println("PING");
+      stub.withDeadlineAfter(10, TimeUnit.SECONDS).ping(PingRequest.newBuilder().build(), new StreamObserver<PingResponse>() {
         @Override
         public void onNext(PingResponse value) {
+          // System.out.println("ON_NEXT");
           available.set(true);
         }
 
         @Override
         public void onError(Throwable t) {
+          // System.out.println("ON_ERROR " + t.getMessage());
           done.countDown();
         }
 
         @Override
         public void onCompleted() {
+          // System.out.println("ON_COMPLETE");
           done.countDown();
         }
       });
-      Utils.resetIfInterrupted(() -> done.await(1_000, TimeUnit.MILLISECONDS));
+      Utils.resetIfInterrupted(() -> done.await());
       return available.get();
     }
 
