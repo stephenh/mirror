bd76b926f02ab26cf61e5b152d2f11feb1019573
Stephen Haberman <stephen.haberman@gmail.com>
2017-09-07 13:10:22 -0500

    Unbound the queues since things are pretty stable.

--- src/main/java/mirror/Queues.java
+++ src/main/java/mirror/Queues.java
@@ -6,12 +6,12 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class Queues {
 
   // this is probably too large, but hopefully it will avoid inotify overflow exceptions
-  public final BlockingQueue<Update> incomingQueue = new LinkedBlockingQueue<>(1_000_000);
+  public final BlockingQueue<Update> incomingQueue = new LinkedBlockingQueue<>();
   // These need to be large enough so that when the SyncLogic thread is outputting the
   // results of a diff, and there are ~10k/100k new files to send, that it can completely
   // offload those Updates to the saveToRemote queue, so that it can unblock, and start
   // accepting new Updates from the incomingQueue.
-  public final BlockingQueue<Update> saveToLocal = new LinkedBlockingQueue<>(1_000_000);
-  public final BlockingQueue<Update> saveToRemote = new LinkedBlockingQueue<>(1_000_000);
+  public final BlockingQueue<Update> saveToLocal = new LinkedBlockingQueue<>();
+  public final BlockingQueue<Update> saveToRemote = new LinkedBlockingQueue<>();
 
 }
